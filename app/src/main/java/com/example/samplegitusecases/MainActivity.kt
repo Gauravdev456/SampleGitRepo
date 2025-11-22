5e1d199db92a5a636cd04e5ab4c1e8da056e43be
Gauravdev Hb-GAURAVHB <gauravhb@motorola.com>
2025-11-22 16:16:13 +0530

    iuiiiiiiiiii

--- app/src/main/java/com/example/samplegitusecases/MainActivity.kt
+++ app/src/main/java/com/example/samplegitusecases/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SampleGitUsecasesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android4",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
